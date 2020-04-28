e49cd4c56b7282e423ea722b98c8763594234427
JhoselynQV <jhoselyn.quispe@upeu.edu.pe>
2020-04-28 16:31:05 +0000

    commit

---
+++ Git.py
@@ -0,0 +1,24 @@
+print("Hola Mundo")
+a=12
+d=14
+print("Resultado Suma a+d",(a+d))
+
+a=1
+e=2
+i=3
+o=4
+u=5
+print("Resultado Suma a+e+i+o+u",(a+e+i+o+u))
+
+u=5
+k=6
+print("Resultado Multiplicacion u*k",(u*k))
+
+y=100
+x=5
+print("Resultado Division y/x",(y/x))
+
+z=50
+x=2
+v=5
+print("resultado Division,Multiplicacion z/x*v",(z/x*v))
