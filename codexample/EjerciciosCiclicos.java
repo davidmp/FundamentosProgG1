50cab89b17b07b8b7c9a287107efbd7542986faf
repl.it user <replituser@example.com>
2020-05-28 13:52:42 +0000

    Factorial numero Grande2

---
+++ codexample/EjerciciosCiclicos.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class EjerciciosCiclicos{
+  static Scanner input= new Scanner(System.in);
+
+  public static void calcularFactorialNNum(){
+    BigInteger factorial = BigInteger.valueOf(1);
+    int numero;
+    System.out.print("Ingrese el factorial un Numero:");
+    numero=input.nextInt();
+    while(numero!= 0) {
+        //factorial=(Long)factorial*numero;
+        factorial=factorial.multiply(BigInteger.valueOf(numero));
+        numero--;//numero=numero-3; numero-= 2
+    }
+    System.out.println("El factorial es: "+ factorial); 
+  }
+
+  public static void main(String[] arg){     
+    System.out.println("Probar Algoritmos: ");
+    
+    String opcion="SI";
+    int numAlg;
+    do{
+      System.out.println("Ingrese el Algoritmo que desea probar: ");
+      numAlg=input.nextInt();
+      switch(numAlg){
+        case 1: calcularFactorialNNum();   break;
+        case 2:    break;
+        case 3:    break;
+        default: System.out.println("Num de Algoritmo no existe!!"); break;
+      }
+      System.out.println("Desea probar mas algoritmos? SI/NO");
+      opcion=input.next();
+
+    }while(opcion.equals("SI"));
+
+  }
+
+}
