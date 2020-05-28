620bad359b236987d009ea1a28db76327df4d18e
repl.it user <replituser@example.com>
2020-05-28 14:26:37 +0000

    CalcularMediaAritmeticaGometrica

--- codexample/EjerciciosCiclicos.java
+++ codexample/EjerciciosCiclicos.java
@@ -6,19 +6,39 @@ class EjerciciosCiclicos{
 
   public static void calcularFactorialNNum(){
     BigInteger factorial = BigInteger.valueOf(1);
-    int factorial = 1;
+   
     int numero;
     System.out.print("Ingrese el factorial un Numero:");
     numero=input.nextInt();
     while(numero!= 0) {
         //factorial=(Long)factorial*numero;
-        factorial=factorial.multiply(BigInteger.valueOf(numero));
-        factorial*=numero;
-
+        factorial=factorial.multiply(BigInteger.valueOf(numero));        
         numero--;//numero=numero-3; numero-= 2
     }
     System.out.println("El factorial es: "+ factorial); 
   }
+
+  public static void calcularMediaGeoArit(){
+    int cantNumeros;
+    double mediaArit=0, mediaGeo=1, sumTotalNum;
+    System.out.print("Ingrese la cantidad de nuemros para la operacion:");
+    cantNumeros=input.nextInt();
+    for(int contador=1;contador<=cantNumeros;contador++){
+      System.out.print("Ingrese el valor de la posicion "+contador+":");
+      double numeroLeido=input.nextDouble();
+      mediaGeo=mediaGeo*numeroLeido;
+      mediaArit=mediaArit+numeroLeido;
+    }
+    mediaGeo=Math.sqrt(mediaGeo);
+    mediaArit=mediaArit/cantNumeros;
+    if(mediaGeo<mediaArit){
+System.out.print("El valor Geometrico es Menor con el valor de:"+mediaGeo+":");
+    }else{
+System.out.print("El valor Aritmetico es Menor con el valor de:"+mediaArit+":");
+    }
+
+  }
+
 
   public static void main(String[] arg){     
     System.out.println("Probar Algoritmos: ");
@@ -30,7 +50,7 @@ class EjerciciosCiclicos{
       numAlg=input.nextInt();
       switch(numAlg){
         case 1: calcularFactorialNNum();   break;
-        case 2:    break;
+        case 2: calcularMediaGeoArit();   break;
         case 3:    break;
         default: System.out.println("Num de Algoritmo no existe!!"); break;
       }
