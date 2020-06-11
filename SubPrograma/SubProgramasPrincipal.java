56a1f9693e61c0e869cc4e47e5c6ca897d7717db
davidmp <mamanipari@gmail.com>
2020-06-11 11:21:24 -0500

    Ejemplos de Subprogramas y Empaquetado

--- SubPrograma/SubProgramasPrincipal.java
+++ SubPrograma/SubProgramasPrincipal.java
@@ -6,17 +6,31 @@ public class SubProgramasPrincipal{
     //javac -d build/ upeu\*.java *.java
     //cd build
     //java -cp . SubProgramasPrincipal
+    //Empaquetado
+    //jar cvfm Programa.jar manifest.mf upeu/edu/pe/util/*.class upeu/edu/pe/*.class *.class
     public static void main(String[] args) {
         LeerTeclado teclado=new LeerTeclado();
-        System.out.println("Saludos");
-       /* int a=teclado.leer(0, "Ingrese el Primer Valor");
-        int b=teclado.leer(0, "Ingrese el Segundo Valor Valor");
-        System.out.println("El Resultado de la Suma es:"+(a+b));*/
-
         EjerciciosJava obj=new EjerciciosJava();
-        int a=teclado.leer(0, "Ingrese el Numero Inicial:");
-        int b=teclado.leer(0, "Ingrese el Numero Final:");        
-        obj.factorialEntreRangoNumeros(a, b);
+        String opcion="SI";
+        int numAlg;
+        do{
+
+          numAlg=teclado.leer(0, "Ingrese el Algoritmo que desea probar: ");
+          switch(numAlg){
+            case 1:         
+            int a=teclado.leer(0, "Ingrese el Numero Inicial:");
+            int b=teclado.leer(0, "Ingrese el Numero Final:");        
+            obj.factorialEntreRangoNumeros(a, b);   
+            break;
+            case 2:   break;
+            case 3:   break;
+            case 4:   break;
+            default: System.out.println("Num de Algoritmo no existe!!"); break;
+          }
+
+          opcion=teclado.leer("", "Desea probar mas algoritmos? SI/NO");
+    
+        }while(opcion.equals("SI"));        
 
     }
 }
