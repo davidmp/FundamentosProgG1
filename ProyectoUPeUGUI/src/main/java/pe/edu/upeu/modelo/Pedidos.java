877433512564cea360243da57d30118070c109a8
davidmp <mamanipari@gmail.com>
2020-07-16 08:22:37 -0500

    Pedidos Modelo

---
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/modelo/Pedidos.java
@@ -0,0 +1,60 @@
+package pe.edu.upeu.modelo;
+
+public class Pedidos {
+    public String pedidoId;
+    public String productoId;
+    public String descripcionPed;
+    public double cantidad;
+    public double precioUnit;
+    public double precioTotal;
+
+    public String getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(String pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public String getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(String productoId) {
+        this.productoId = productoId;
+    }
+
+    public String getDescripcionPed() {
+        return descripcionPed;
+    }
+
+    public void setDescripcionPed(String descripcionPed) {
+        this.descripcionPed = descripcionPed;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnit() {
+        return precioUnit;
+    }
+
+    public void setPrecioUnit(double precioUnit) {
+        this.precioUnit = precioUnit;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    
+}
